99fef5b6e0816e2ea49ff76935bd7ea537c1ec82
EvanderVegt <e.vandervegt03@gmail.com>
2017-11-02 21:46:28 +0100

    zeeslag stap 1

--- index.php
+++ index.php
@@ -17,131 +17,140 @@
     </head>
     <body>
         <center>
+            <p  style=background-color:powderblue; style=color:black; id="table"></p>
             <table id="table">
                 
                     <tr>
-                        <td id="td1" onclick="content()">x</td>
-                        <td id="td2">x</td>
-                        <td id="td3">x</td>
-                        <td id="td4">0</td>
-                        <td id="td5">0</td>
-                        <td id="td6">0</td>
-                        <td id="td7">0</td>
-                        <td id="td8">0</td>
-                        <td id="td9">0</td>
-                        <td id="td10">0</td>
+                        <td id="td1" onclick="content(this)">x</td>
+                        <td id="td2" onclick="content(this)">x</td>
+                        <td id="td3" onclick="content(this)">x</td>
+                        <td id="td4" onclick="content(this)">0</td>
+                        <td id="td5" onclick="content(this)">0</td>
+                        <td id="td6" onclick="content(this)">0</td>
+                        <td id="td7" onclick="content(this)">0</td>
+                        <td id="td8" onclick="content(this)">0</td>
+                        <td id="td9" onclick="content(this)">0</td>
+                        <td id="td10" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td11">0</td>
-                        <td id="td12">0</td>
-                        <td id="td13">0</td>
-                        <td id="td14">0</td>
-                        <td id="td15">0</td>
-                        <td id="td16">0</td>
-                        <td id="td17">0</td>
-                        <td id="td18">0</td>
-                        <td id="td19">0</td>
-                        <td id="td20">0</td>
+                        <td id="td11" onclick="content(this)">0</td>
+                        <td id="td12" onclick="content(this)">0</td>
+                        <td id="td13" onclick="content(this)">0</td>
+                        <td id="td14" onclick="content(this)">0</td>
+                        <td id="td15" onclick="content(this)">0</td>
+                        <td id="td16" onclick="content(this)">0</td>
+                        <td id="td17" onclick="content(this)">0</td>
+                        <td id="td18" onclick="content(this)">0</td>
+                        <td id="td19" onclick="content(this)">0</td>
+                        <td id="td20" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td21">0</td>
-                        <td id="td22">0</td>
-                        <td id="td23">0</td>
-                        <td id="td24">0</td>
-                        <td id="td25">0</td>
-                        <td id="td26">0</td>
-                        <td id="td27">0</td>
-                        <td id="td28">0</td>
-                        <td id="td29">0</td>
-                        <td id="td30">0</td>
+                        <td id="td21" onclick="content(this)">0</td>
+                        <td id="td22" onclick="content(this)">0</td>
+                        <td id="td23" onclick="content(this)">0</td>
+                        <td id="td24" onclick="content(this)">0</td>
+                        <td id="td25" onclick="content(this)">0</td>
+                        <td id="td26" onclick="content(this)">0</td>
+                        <td id="td27" onclick="content(this)">0</td>
+                        <td id="td28" onclick="content(this)">0</td>
+                        <td id="td29" onclick="content(this)">0</td>
+                        <td id="td30" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td31">0</td>
-                        <td id="td32">0</td>
-                        <td id="td33">0</td>
-                        <td id="td34">0</td>
-                        <td id="td35">0</td>
-                        <td id="td36">0</td>
-                        <td id="td37">0</td>
-                        <td id="td38">0</td>
-                        <td id="td39">0</td>
-                        <td id="td40">0</td>
+                        <td id="td31" onclick="content(this)">0</td>
+                        <td id="td32" onclick="content(this)">0</td>
+                        <td id="td33" onclick="content(this)">0</td>
+                        <td id="td34" onclick="content(this)">0</td>
+                        <td id="td35" onclick="content(this)">0</td>
+                        <td id="td36" onclick="content(this)">0</td>
+                        <td id="td37" onclick="content(this)">0</td>
+                        <td id="td38" onclick="content(this)">0</td>
+                        <td id="td39" onclick="content(this)">0</td>
+                        <td id="td40" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td41">0</td>
-                        <td id="td42">0</td>
-                        <td id="td43">0</td>
-                        <td id="td44">0</td>
-                        <td id="td45">0</td>
-                        <td id="td46">0</td>
-                        <td id="td47">0</td>
-                        <td id="td48">0</td>
-                        <td id="td49">0</td>
-                        <td id="td50">0</td>
+                        <td id="td41" onclick="content(this)">0</td>
+                        <td id="td42" onclick="content(this)">0</td>
+                        <td id="td43" onclick="content(this)">0</td>
+                        <td id="td44" onclick="content(this)">0</td>
+                        <td id="td45" onclick="content(this)">0</td>
+                        <td id="td46" onclick="content(this)">0</td>
+                        <td id="td47" onclick="content(this)">0</td>
+                        <td id="td48" onclick="content(this)">0</td>
+                        <td id="td49" onclick="content(this)">0</td>
+                        <td id="td50" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td51">0</td>
-                        <td id="td52">0</td>
-                        <td id="td53">0</td>
-                        <td id="td54">0</td>
-                        <td id="td55">0</td>
-                        <td id="td56">0</td>
-                        <td id="td57">0</td>
-                        <td id="td58">0</td>
-                        <td id="td59">0</td>
-                        <td id="td60">0</td>
+                        <td id="td51" onclick="content(this)">0</td>
+                        <td id="td52" onclick="content(this)">0</td>
+                        <td id="td53" onclick="content(this)">0</td>
+                        <td id="td54" onclick="content(this)">0</td>
+                        <td id="td55" onclick="content(this)">0</td>
+                        <td id="td56" onclick="content(this)">0</td>
+                        <td id="td57" onclick="content(this)">0</td>
+                        <td id="td58" onclick="content(this)">0</td>
+                        <td id="td59" onclick="content(this)">0</td>
+                        <td id="td60" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td61">0</td>
-                        <td id="td62">0</td>
-                        <td id="td63">0</td>
-                        <td id="td64">0</td>
-                        <td id="td65">0</td>
-                        <td id="td66">0</td>
-                        <td id="td67">0</td>
-                        <td id="td68">0</td>
-                        <td id="td69">0</td>
-                        <td id="td70">0</td>
+                        <td id="td61" onclick="content(this)">0</td>
+                        <td id="td62" onclick="content(this)">0</td>
+                        <td id="td63" onclick="content(this)">0</td>
+                        <td id="td64" onclick="content(this)">0</td>
+                        <td id="td65" onclick="content(this)">0</td>
+                        <td id="td66" onclick="content(this)">0</td>
+                        <td id="td67" onclick="content(this)">0</td>
+                        <td id="td68" onclick="content(this)">0</td>
+                        <td id="td69" onclick="content(this)">0</td>
+                        <td id="td70" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td71">0</td>
-                        <td id="td72">0</td>
-                        <td id="td73">0</td>
-                        <td id="td74">0</td>
-                        <td id="td75">0</td>
-                        <td id="td76">0</td>
-                        <td id="td77">0</td>
-                        <td id="td78">0</td>
-                        <td id="td79">0</td>
-                        <td id="td80">0</td>
+                        <td id="td71" onclick="content(this)">0</td>
+                        <td id="td72" onclick="content(this)">0</td>
+                        <td id="td73" onclick="content(this)">0</td>
+                        <td id="td74" onclick="content(this)">0</td>
+                        <td id="td75" onclick="content(this)">0</td>
+                        <td id="td76" onclick="content(this)">0</td>
+                        <td id="td77" onclick="content(this)">0</td>
+                        <td id="td78" onclick="content(this)">0</td>
+                        <td id="td79" onclick="content(this)">0</td>
+                        <td id="td80" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td81">0</td>
-                        <td id="td82">0</td>
-                        <td id="td83">0</td>
-                        <td id="td84">0</td>
-                        <td id="td85">0</td>
-                        <td id="td86">0</td>
-                        <td id="td87">0</td>
-                        <td id="td88">0</td>
-                        <td id="td89">0</td>
-                        <td id="td90">0</td>
+                        <td id="td81" onclick="content(this)">0</td>
+                        <td id="td82" onclick="content(this)">0</td>
+                        <td id="td83" onclick="content(this)">0</td>
+                        <td id="td84" onclick="content(this)">0</td>
+                        <td id="td85" onclick="content(this)">0</td>
+                        <td id="td86" onclick="content(this)">0</td>
+                        <td id="td87" onclick="content(this)">0</td>
+                        <td id="td88" onclick="content(this)">0</td>
+                        <td id="td89" onclick="content(this)">0</td>
+                        <td id="td90" onclick="content(this)">0</td>
                     </tr>
                     <tr>
-                        <td id="td91">0</td>
-                        <td id="td92">0</td>
-                        <td id="td93">0</td>
-                        <td id="td94">0</td>
-                        <td id="td95">0</td>
-                        <td id="td96">0</td>
-                        <td id="td97">0</td>
-                        <td id="td98">0</td>
-                        <td id="td99">0</td>
-                        <td id="td100">0</td>
+                        <td id="td91" onclick="content(this)">0</td>
+                        <td id="td92" onclick="content(this)">0</td>
+                        <td id="td93" onclick="content(this)">0</td>
+                        <td id="td94" onclick="content(this)">0</td>
+                        <td id="td95" onclick="content(this)">0</td>
+                        <td id="td96" onclick="content(this)">0</td>
+                        <td id="td97" onclick="content(this)">0</td>
+                        <td id="td98" onclick="content(this)">0</td>
+                        <td id="td99" onclick="content(this)">0</td>
+                        <td id="td100" onclick="content(this)">0</td>
                     </tr>
 
                 </tbody>
             </table>
-            </center
+            <script>
+            function content(elem){
+                elem.style.backgroundColor = "red";
+                
+                var p = document.getElementById("table");
+                p.innerHTML ='Je hebt mis geschoten!'+' '+elem.innerText;
+            }
+            </script>
+        </center>
     </body>
 </html>
